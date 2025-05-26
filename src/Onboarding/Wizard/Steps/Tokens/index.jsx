1c0119eb7fe741c0a9e3f044cf98f1186cea6df0
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2025-05-26 02:10:21 +0200

    Feature: Updated settings interface. (#13)

--- src/Onboarding/Wizard/Steps/Tokens/index.jsx
+++ src/Onboarding/Wizard/Steps/Tokens/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 
-import './tokens.scss'
+import './tokens.pcss'
 
 const Tokens = ( { setStep } ) => {
 	const [ token, setToken ] = useState( '' );
