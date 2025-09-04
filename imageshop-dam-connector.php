ab0472803e693cb980c01f8da508f878c418ce20
Marius Jensen <marius@clorith.net>
2025-09-04 13:48:00 +0200

    Tooling: Introduce Laravel-inspired `dd()` function for quick development troubleshooting in limited environments

--- imageshop-dam-connector.php
+++ imageshop-dam-connector.php
@@ -52,7 +52,11 @@ namespace Imageshop\WordPress;
 		require $base_dir . $file;
 	}
 } );
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
 
+require_once __DIR__ . '/includes/helpers.php';
 require_once __DIR__ . '/includes/compatibility.php';
 
 require_once __DIR__ . '/includes/class-imageshop.php';
---
+++ includes/helpers.php
@@ -0,0 +1,16 @@
+<?php
+// Helper functions aimed at improving developer experiences.
+
+if ( ! function_exists( 'dd' ) ) {
+	function dd() {
+		if ( ! function_exists( 'dump' ) ) {
+			echo '<pre>';
+			print_r( func_get_args() );
+			echo '</pre>';
+		} else {
+			call_user_func_array( 'dump', func_get_args() );
+		}
+
+		die();
+	}
+}
